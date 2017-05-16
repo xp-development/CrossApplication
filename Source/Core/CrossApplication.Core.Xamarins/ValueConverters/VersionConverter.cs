1a1af851d515b223f0ac9ccda07a0b9e52db3da8
dev <dev@mail.com>
2017-05-16 18:52:09 +0100

    Restructure project locations.

---
+++ Source/Core/CrossApplication.Core.Xamarins/ValueConverters/VersionConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace CrossApplication.Core.Xamarins.ValueConverters
+{
+  public class VersionConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (!(value is Version))
+      {
+        return value;
+      }
+
+      return ((Version) value).ToString();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
