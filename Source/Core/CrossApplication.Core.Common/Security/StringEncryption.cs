fb26637c46bb4b0abffe9db16bb4ed2481839bb7
dev <dev@mail.com>
2018-02-09 19:26:56 +0100

    Use ProtectData to save passwords.

--- Source/Core/CrossApplication.Core.Common/Security/StringEncryption.cs
+++ Source/Core/CrossApplication.Core.Common/Security/StringEncryption.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 using System.Text;
 using CrossApplication.Core.Contracts.Security;
 
@@ -8,28 +9,12 @@ namespace CrossApplication.Core.Common.Security
     {
         public string Encrypt(string value)
         {
-            var result = new StringBuilder();
-            foreach (var t in value)
-            {
-                var first = (byte) ((t & 0xFF00) >> 8);
-                var second = (byte) (t & 0xFF);
-                result.Append(BitConverter.ToChar(new []{(byte) (((byte) (first & 0xF) << 4) | (byte)(second & 0xF)), (byte) (((byte)(second >> 4) << 4) | (byte)(first >> 4))}, 0));
-            }
-
-            return result.ToString();
+            return Encoding.Unicode.GetString(ProtectedData.Protect(Encoding.Unicode.GetBytes(value), null, DataProtectionScope.CurrentUser));
         }
 
         public string Decrypt(string value)
         {
-            var result = new StringBuilder();
-            foreach (var t in value)
-            {
-                var first = (byte)((t & 0xFF00) >> 8);
-                var second = (byte)(t & 0xFF);
-                result.Append(BitConverter.ToChar(new[] { (byte)((first >> 4 << 4) | second & 0xF), (byte)(((byte)(first & 0xF) << 4) | second >> 4) }, 0));
-            }
-
-            return result.ToString();
+            return Encoding.Unicode.GetString(ProtectedData.Unprotect(Encoding.Unicode.GetBytes(value), null, DataProtectionScope.CurrentUser));
         }
     }
 }
